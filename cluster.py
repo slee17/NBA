cb603bfa4651b35e36cd86b3bc03f70865e87179
Rachel Lee <slee17@cmc.edu>
2017-06-11 21:32:05 +0900

    Clean up cluster.py

--- cluster.py
+++ cluster.py
@@ -9,71 +9,52 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == '__main__':
-	# data
 	data = read_csv('Seasons_Stats.csv')
+	
+	# clean up
+	data = data.loc[data["Year"] > 2016]
+	data = data.fillna(0)
+
+	# for labeling later
 	years = data["Year"].tolist()
 	names = data["Player"].tolist()
-	names_years = []
-	for i in range(len(names)):
-		names_years.append("%s %f" % (names[i], years[i]))
-	# only keep stats after 1999
-	data = data.loc[data["Year"] > 2015]
-	
+	names_years = ["%s %.0f" % pair for pair in zip(names, years)]
+
 	# drop non-continuous variables
 	data.drop(["Unnamed: 0", "Year", "Player", "Pos", "Tm"], axis=1, inplace=True)
-	data = data.fillna(0)
+	
 	data = data[["3P", "3PA", "3P%"]]
 	data = data.as_matrix().astype("float32", copy=False)
+
 	# normalize
 	# stscaler = StandardScaler().fit(data)
 	# data = stscaler.transform(data)
 
 	# cluster
-	db = DBSCAN(eps=3, metric='euclidean', min_samples=2)
+	db = DBSCAN(eps=10, metric='euclidean', min_samples=10)
 	db.fit(data)
-	print (db)
-
+	
 	print (Counter(db.labels_))
-	print (db.labels_)
-
+	
+	# http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#sphx-glr-auto-examples-cluster-plot-dbscan-py
 	core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 	core_samples_mask[db.core_sample_indices_] = True
 	labels = db.labels_
 
-	# Number of clusters in labels, ignoring noise if present.
+	# number of clusters in labels, ignoring noise if present
 	n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
-	print('Estimated number of clusters: %d' % n_clusters_)
-	# print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-	# print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-	# print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-	# print("Adjusted Rand Index: %0.3f"
-	#       % metrics.adjusted_rand_score(labels_true, labels))
-	# print("Adjusted Mutual Information: %0.3f"
-	#       % metrics.adjusted_mutual_info_score(labels_true, labels))
-	# print("Silhouette Coefficient: %0.3f"
-	#       % metrics.silhouette_score(X, labels))
+	print("Number of clusters: %d" % n_clusters_)
+	print("Silhouette Coefficient: %0.3f"
+	      % metrics.silhouette_score(data, labels))
 
-	# visualization
-	# pca = PCA(n_components=2).fit(data)
-	# pca_2d = pca.transform(data)
-	# for i in range(0, pca_2d.shape[0]):
-	# 	if db.labels_[i] == 0:
-	# 		c1 = plt.scatter(pca_2d[i,0], pca_2d[i,1], c='r', marker='+')
-	# 	# elif db.labels_[i] == 1:
-	# 	# 	c2 = plt.scatter(pca_2d[i,0], pca_2d[i,1], c='g', marker='o')
-	# 	elif db.labels_[i] == -1:
-	# 		c3 = plt.scatter(pca_2d[i,0], pca_2d[i,1], c='b', marker='*')
-	# plt.legend([c1, c3], ['Cluster 1', 'Noise'])
-	# plt.title('DBSCAN Results')
-	# plt.show()
-	# Black removed and is used for noise instead.
+	# black removed and is used for noise instead
 	labels = db.labels_
 	unique_labels = set(labels)
 	colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
 	for k, col in zip(unique_labels, colors):
 	    if k == -1:
-	        # Black used for noise.
+	        # black used for noise
 	        col = 'k'
 
 	    class_member_mask = (labels == k)
@@ -86,13 +67,13 @@ if __name__ == '__main__':
 	    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
 	             markeredgecolor='k', markersize=6)
 
-	for name, x, y in zip(names_years, data[:, 0], data[:, 1]):
-	    plt.annotate(
-	        name,
-	        xy=(x, y), xytext=(-20, 20),
-	        textcoords='offset points', ha='right', va='bottom',
-	        bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
-	        arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
+	# for name_year, x, y in zip(names_years, data[:, 0], data[:, 1]):
+	#     plt.annotate(
+	#         name_year,
+	#         xy=(x, y), xytext=(-10, 10),
+	#         textcoords='offset points', ha='right', va='bottom',
+	#         bbox=dict(boxstyle='round, pad=0.5', fc='skyblue', alpha=0.5),
+	#         arrowprops=dict(arrowstyle='-', connectionstyle='arc3, rad=0'))
 
-	plt.title('Estimated number of clusters: %d' % n_clusters_)
+	plt.title('NBA Players Stats')
 	plt.show()
