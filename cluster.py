f2b93f63d207ee31ca38b74ce082e1471d8a3a44
Rachel Lee <slee17@cmc.edu>
2017-06-12 02:46:19 +0900

    Add code for mvp stat visualization, add graphs, and clean up

--- cluster.py
+++ cluster.py
@@ -7,12 +7,13 @@ from collections import Counter
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-if __name__ == '__main__':
-	data = read_csv('Seasons_Stats.csv')
-	
+def db_cluster(path, year, cols):
+	data = read_csv(path)
+
 	# clean up
-	data = data.loc[data["Year"] > 2016]
+	data = data.loc[data["Year"] > year]
 	data = data.fillna(0)
 
 	# for labeling later
@@ -23,7 +24,7 @@ if __name__ == '__main__':
 	# drop non-continuous variables
 	data.drop(["Unnamed: 0", "Year", "Player", "Pos", "Tm"], axis=1, inplace=True)
 	
-	data = data[["3P", "3PA", "3P%"]]
+	data = data[cols]
 	data = data.as_matrix().astype("float32", copy=False)
 
 	# normalize
@@ -36,9 +37,6 @@ if __name__ == '__main__':
 	
 	print (Counter(db.labels_))
 	
-	# http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#sphx-glr-auto-examples-cluster-plot-dbscan-py
-	core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-	core_samples_mask[db.core_sample_indices_] = True
 	labels = db.labels_
 
 	# number of clusters in labels, ignoring noise if present
@@ -48,32 +46,87 @@ if __name__ == '__main__':
 	print("Silhouette Coefficient: %0.3f"
 	      % metrics.silhouette_score(data, labels))
 
-	# black removed and is used for noise instead
+	plot_clusters(data, db, names_years, "NBA Players Stats Clustered by: " + ", ".join(cols))
+
+def db_cluster_mvp(season_path, mvp_path, cols):
+	season_data = read_csv("./Data/season_stats.csv")
+	mvp_data = read_csv("./Data/mvp.csv")
+	data = pd.merge(mvp_data, season_data, how="inner", left_on=["Year", "Player"], right_on=["Year", "Player"])
+	
+	data = data.fillna(0)
+
+	# for labeling later
+	years = data["Year"].tolist()
+	names = data["Player"].tolist()
+	names_years = ["%s %.0f" % pair for pair in zip(names, years)]
+
+	data = data[cols]
+	data = data.as_matrix().astype("float32", copy=False)
+
+	# normalize
+	# stscaler = StandardScaler().fit(data)
+	# data = stscaler.transform(data)
+
+	# cluster
+	db = DBSCAN(eps=100, metric='euclidean', min_samples=2)
+	db.fit(data)
+	
+	print (Counter(db.labels_))
+	
+	labels = db.labels_
+
+	# number of clusters in labels, ignoring noise if present
+	n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+	print("Number of clusters: %d" % n_clusters_)
+	print("Silhouette Coefficient: %0.3f"
+	      % metrics.silhouette_score(data, labels))
+
+	plot_clusters(data, db, names_years, "NBA MVP Stats Clustered by: " + ", ".join(cols))
+
+def plot_clusters(data, db, annotations, title, show_label=True):
+	"""Function for plotting clusters. Largely taken from:
+	http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#sphx-glr-auto-examples-cluster-plot-dbscan-py
+	"""
+	core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+	core_samples_mask[db.core_sample_indices_] = True
+
 	labels = db.labels_
 	unique_labels = set(labels)
 	colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+	
 	for k, col in zip(unique_labels, colors):
 	    if k == -1:
-	        # black used for noise
+	        # use black for noise
 	        col = 'k'
 
 	    class_member_mask = (labels == k)
 
 	    xy = data[class_member_mask & core_samples_mask]
+	    
 	    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
 	             markeredgecolor='k', markersize=14)
 
-	    xy = data[class_member_mask & ~core_samples_mask]
+	    xy = data[class_member_mask]
 	    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
 	             markeredgecolor='k', markersize=6)
 
-	# for name_year, x, y in zip(names_years, data[:, 0], data[:, 1]):
-	#     plt.annotate(
-	#         name_year,
-	#         xy=(x, y), xytext=(-10, 10),
-	#         textcoords='offset points', ha='right', va='bottom',
-	#         bbox=dict(boxstyle='round, pad=0.5', fc='skyblue', alpha=0.5),
-	#         arrowprops=dict(arrowstyle='-', connectionstyle='arc3, rad=0'))
+	if show_label:
+		for annotation, x, y in zip(annotations, data[:, 0], data[:, 1]):
+		    plt.annotate(
+		        annotation,
+		        xy=(x, y), xytext=(-3, 3), fontsize=6,
+		        textcoords='offset points', ha='right', va='bottom',
+		        bbox=dict(boxstyle='round, pad=0.2', fc='skyblue', alpha=0.5),
+		        arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=0'))
 
-	plt.title('NBA Players Stats')
+	plt.title(title)
 	plt.show()
+
+if __name__ == "__main__":
+	free_throws = ["FT", "FTA", "FT%"]
+	threes = ["3P", "3PA", "3P%"]
+	all = ["FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA", "2P%", "eFG%"]
+	
+	# db_cluster("./Data/season_stats.csv", 2016, free_throws)
+	db_cluster_mvp("./Data/season_stats.csv", "./Data/mvp.csv", all)
