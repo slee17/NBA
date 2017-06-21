c8b158f4047a6249c37287b2a126abaab7cf4159
Rachel Lee <slee17@cmc.edu>
2017-06-22 02:48:53 +0900

    Update code for title generation

--- cluster.py
+++ cluster.py
@@ -29,22 +29,38 @@ def cluster(alg, path, cols, mvp_path=None, start_year=None, players=None, show_
 	if players:
 		data = data.loc[data["Player"].isin(players)]
 		# TODO: throw exception if mvp_path != None and player not an MVP
-	data = data.fillna(0)
+	# handle rows with nan - could also be implemented by data.fillna(0) but that would
+	# contaminate the dataset
+	data = data.dropna()
 
-	# for labeling later
-	years = data["Year"].tolist()
+	# for labeling and title later
 	names = data["Player"].tolist()
-	names_years = ["%s %.0f" % pair for pair in zip(names, years)]
+	try:
+		years = data["Year"].tolist()
+		labels = ["%s %.0f" % pair for pair in zip(names, years)]
+	except KeyError:
+		labels = names
 
-	# for title
+	players_plotted = "NBA Players"
+	if mvp_path:
+		players_plotted = "NBA MVPs"
+	if players:
+		players_plotted = ", ".join(players)
+
 	if not start_year:
-		start_year = data['Year'].min(axis=0)
+		try:
+			start_year = data['Year'].min(axis=0)
+			title = "%s Stats from %d on Clustered by %s using %s" % (players_plotted, start_year+1, ", ".join(cols), alg.upper())
+		except KeyError:
+			labels = names
+			title = "%s Stats Clustered by %s using %s" % (players_plotted, ", ".join(cols), alg.upper())
 
+	# keep only columns of interest
 	data = data[cols]
 	data = data.as_matrix().astype("float32", copy=False)
 
 	# normalize
-	data = StandardScaler().fit_transform(data)
+	# data = StandardScaler().fit_transform(data)
 	
 	# use the indicated clustering algorithm
 	alg = alg.lower()
@@ -79,7 +95,7 @@ def cluster(alg, path, cols, mvp_path=None, start_year=None, players=None, show_
 		i = 0
 		for unique_label in unique_labels:
 			pl.scatter(pca_2d[:, 0], pca_2d[:, 1], c=clusterer.labels_)
-			for name_year, x, y in zip(names_years, pca_2d[:, 0], pca_2d[:, 1]):            
+			for name_year, x, y in zip(labels, pca_2d[:, 0], pca_2d[:, 1]):            
 				if clusterer.labels_[i] == unique_label:
 					pl.annotate(
 						name_year,
@@ -89,25 +105,19 @@ def cluster(alg, path, cols, mvp_path=None, start_year=None, players=None, show_
 						arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=0'))
 				i += 1
 			i = 0
-			players_plotted = "NBA Players"
-			if mvp_path:
-				players_plotted = "NBA MVPs"
-			if players:
-				players_plotted = ", ".join(players)
-			pl.title("%s Stats from %d on Clustered by %s using %s"
-				% (players_plotted, start_year+1, ", ".join(cols), alg.upper()))
+			pl.title(title)
 			pl.show()
 
 if __name__ == "__main__":
-	categories = {}
-	categories['free_throws'] = ["FT", "FTA", "FT%"]
-	categories['threes'] = ["3P", "3PA", "3P%"]
-	categories['all'] = ["FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA", "2P%", "eFG%"]
-	# categories['player_info'] = ["height", "weight", "born"]
+	cluster("dbscan", "./Data/players.csv", ["height", "weight"])
 
-	for name, category in categories.items():
+	# categories = {}
+	# categories['free_throws'] = ["FT", "FTA", "FT%"]
+	# categories['threes'] = ["3P", "3PA", "3P%"]
+	# categories['all'] = ["FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA", "2P%", "eFG%"]
+	
+	# for name, category in categories.items():
 		# cluster("kmeans", "./Data/season_stats.csv", category, start_year=2016, show_label=True)
 		# cluster("kmeans", "./Data/season_stats.csv", category, mvp_path="Data/mvp.csv", show_label=True)
-		cluster("agglomerative", "./Data/season_stats.csv", category, mvp_path="Data/mvp.csv", show_label=True)
+		# cluster("agglomerative", "./Data/season_stats.csv", category, mvp_path="Data/mvp.csv", show_label=True)
 		# cluster("dbscan", "./Data/season_stats.csv", category, start_year=2016, show_label=True)
-		# db_cluster_mvp("./Data/season_stats.csv", "./Data/mvp.csv", all)
